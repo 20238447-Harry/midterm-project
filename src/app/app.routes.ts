a15fe1dda9b2d52a2259507c80dba215d1a1fa01
Harry Bahatan <20238447@s.ubaguio.edu>
2026-03-05 11:56:29 +0800

    Added components and Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './comps/home/home.component';
+import { InterpolationComponent } from './comps/interpolation/interpolation.component';
+import { PropertyBindingComponent } from './comps/property-binding/property-binding.component';
+import { EventBindingComponent } from './comps/event-binding/event-binding.component';
+import { TwoWayBindingComponent } from './comps/two-way-binding/two-way-binding.component';
+
+export const routes: Routes = [
+
+{ path: '', component: HomeComponent },
+{ path: 'interpolation', component: InterpolationComponent },
+{ path: 'property-binding', component: PropertyBindingComponent },
+{ path: 'event-binding', component: EventBindingComponent },
+{ path: 'two-way-binding', component: TwoWayBindingComponent }
+
+];
