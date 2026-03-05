932788524bf9a4f32bab939aecc965292b8de8fc
Harry Bahatan <20238447@s.ubaguio.edu>
2026-03-05 21:17:57 +0800

    Updated Home, NavBar and Product Components

--- src/app/comps/home/home.component.ts
+++ src/app/comps/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone : true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  appTitle: string = 'Welcome to my Midterm Project';
+  userImageUrl: string = 'https://png.pngtree.com/png-vector/20240723/ourmid/pngtree-cute-cat-cat-kawaii-chibi-drawing-style-cat-cartoon-png-image_12965065.png';
 }
